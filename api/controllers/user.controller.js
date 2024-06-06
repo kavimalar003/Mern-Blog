96235de7641c4f5022502cde2312f3fb3a3fb965
kavimalar003 <kavimalarraj1999@gmail.com>
2024-06-06 15:28:39 +0530

    create and run the server

--- api/controllers/user.controller.js
+++
@@ -1,5 +0,0 @@
-import { request, response } from "express";
-
-export const test = (request,response) => {
-    response.json({message:'API is working!'})
-}
--- api/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { test } from '../controllers/user.controller.js';
-
-const Router = express.Router();
-
-Router.get('/test',test)
-
-export default Router;
