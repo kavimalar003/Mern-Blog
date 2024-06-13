83327565f9faa3c5c9b3eaaf47911240ea9c827a
kavimalar003 <kavimalarraj1999@gmail.com>
2024-06-13 11:48:49 +0530

    add showmore functionality to the posts results of the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,4 +1,4 @@
-import { Table } from 'flowbite-react';
+import { Table} from 'flowbite-react';
 import React from 'react'
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 const DashPost = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts)
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,7 +15,10 @@ const DashPost = () => {
         const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
         if (res.ok) {
-          setUserPosts(data.posts);
+          setUserPosts((prev) => [...prev, ...data.posts]);
+          if (data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +28,24 @@ const DashPost = () => {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -86,6 +108,14 @@ const DashPost = () => {
             </Table.Body>
           ))}
         </Table>
+        {showMore && (
+          <button
+          onClick={handleShowMore}
+          className='w-full text-teal-500 self-center text-sm py-7'
+        >
+          Show more
+        </button>
+        )}
         </>
       ):(
         <p>You have posts yet</p>
