9b1e244deb7f8eebef7d13ca8874e7201fe1f330
kavimalar003 <kavimalarraj1999@gmail.com>
2024-06-14 13:35:42 +0530

    add comments section to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPost from '../components/DashPost';
 import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -27,6 +28,8 @@ export default function Dashboard() {
       {tab === 'posts' && <DashPost/>}
       {/* users */}
       {tab === 'users' && <DashUsers/>}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
     </div>
   );
 }
